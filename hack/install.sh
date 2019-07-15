87d7498aa942348ab72322fd2e18f60b19643daa
Ce Gao <gaoce@caicloud.io>
2019-07-15 10:33:36 +0800

    chore: Add Dockerfile for docker (#72)
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- hack/install.sh
+++ hack/install.sh
@@ -3,6 +3,7 @@
 ROOT=$(dirname ${BASH_SOURCE})/..
 
 cd ${ROOT}
+mkdir -p ${HOME}/.local/share/jupyter/kernels/kubeflow
 rm -r ${HOME}/.local/share/jupyter/kernels/kubeflow
 cp -r ./artifacts ${HOME}/.local/share/jupyter/kernels/kubeflow
 cd - > /dev/null
